f5469d415eaa0d58ddf9edf5dca6fc57687eb7f7
Rafael Gión Muñoz <rgion@cifpfbmoll.eu>
2020-11-20 11:41:04 +0100

    Mejora del ejercicio 12 para dar soporte al apartado b. De todas formas, si le intentamos fastidiar mucho, como por ejemplo poner espacios al final de la frase no la cuenta bien, habría que añadir más condiciones. Pero es suficiente a nivel didáctico.

--- P7E12.py
+++ P7E12.py
@@ -10,11 +10,13 @@
 def cadena(string):
     letras=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
     signos=[".",":",",",";","-"]
-    palabras=1
+    palabras=0
     for i in range(len(string)):
-        if string[i]in(letras)or(signos):
-            if string[i]==" ":
-                palabras+=1
+        if string[i]==" " and (string[i-1] in signos or string[i-1] in letras):
+            palabras+=1
+    if len(string)!=0:#si nos han introducido texto es que al menos tenemos una palabra
+        palabras +=1  # y la última palabra no tiene espacio ya que le damos al retorno de carro
+
     return palabras
 
 string=input("Introduce una frase: ")
